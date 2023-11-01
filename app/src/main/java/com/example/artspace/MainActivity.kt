a3e9b8120610a94ad3156d771b8f6220f5391d61
Nuray <210103331@stu.sdu.edu.kz>
2023-11-01 14:23:28 +0600

    trying to find logic

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
@@ -44,36 +47,61 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
+@Composable
+fun ArtSpaceApp(){
+     var result by remember { mutableStateOf(1) }
+    when(result){
+        1 -> ArtSpace(
+            textlabel = R.string.nature,
+            drawableRes = R.drawable.first,
+            onNextClick = { result = 2 }
+        )
+    }
+}
 
 @Composable
-fun ArtSpaceApp(modifier: Modifier = Modifier){
+fun ArtSpace(textlabel:Int,drawableRes:Int,onNextClick: () -> Unit,modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.first)
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+         Button(onClick = onNextClick){
+            Text(text = stringResource(R.string.next))
+        }
 
 
-    ){
-        Image(
-            painter = image,
-            contentDescription = null,
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .size(150.dp)
-                .padding(top = 20.dp)
-        )
-       Text(
-           text = "Workvds")
-        Text(
-            text = "cds"
-        )
 
+
+        ) {
+            Image(
+                painter = painterResource(drawableRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(top = 20.dp)
+            )
+            Text(
+                text = stringResource(textlabel)
+            )
+            Text(
+                text = "cds"
+            )
+
+
+        }
+        Text(
+            text = ">"
+        )
 
     }
 
 }
-
 
 @Preview(showBackground = true)
 @Composable
