2ccc9f6922286b34600137a5e98fa6d46d51ff36
Nuray <210103331@stu.sdu.edu.kz>
2023-11-01 17:10:09 +0600

    final

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,11 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,18 +57,21 @@ fun ArtSpaceApp(){
     when(result){
         1 -> ArtSpace(
             textlabel = R.string.nature,
+            textAvtor = R.string.natureDescription,
             drawableRes = R.drawable.first,
             onNextClick = { },
             on = {result = 2}
         )
         2 -> ArtSpace(
             textlabel = R.string.dala,
+            textAvtor = R.string.dalaDescription,
             drawableRes = R.drawable.photo,
             onNextClick = { result = 1},
            on = { result = 3}
         )
         3 -> ArtSpace(
             textlabel = R.string.three,
+            textAvtor = R.string.threeDescription,
             drawableRes = R.drawable.third,
             onNextClick = { result = 2 },
             on = { })
@@ -75,7 +82,9 @@ fun ArtSpaceApp(){
 }
 
 @Composable
-fun ArtSpace(textlabel:Int,drawableRes:Int,onNextClick: () -> Unit,on:()->Unit,modifier: Modifier = Modifier) {
+fun ArtSpace(textlabel:Int,textAvtor:Int,drawableRes:Int,onNextClick: () -> Unit,on:()->Unit,modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
@@ -97,20 +106,25 @@ fun ArtSpace(textlabel:Int,drawableRes:Int,onNextClick: () -> Unit,on:()->Unit,m
                 painter = painterResource(drawableRes),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(150.dp)
+                    .size(250.dp)
                     .padding(top = 20.dp)
             )
             Text(
-                text = stringResource(textlabel)
+                text = stringResource(textlabel),
+                color = Color(0xFF272C8C), // Color(0xFFRRGGBB) for the desired color
+                fontSize = 23.sp,
             )
             Text(
-                text = "cds"
+                text = stringResource(textAvtor)
             )
 
 
         }
-        Button(onClick = on){
-            Text(text = stringResource(R.string.nextnext))
+        Button(onClick = on
+
+        ){
+            Text(text = stringResource(R.string.nextnext),
+                )
         }
 
     }
